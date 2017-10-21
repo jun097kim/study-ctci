62822ff9f1521586c4677397cd16c0778aea60a1
jun097kim <jun097kim@gmail.com>
2017-10-22 00:27:42 +0900

    pp. 142~143

---
+++ src/ch01/Q2_05_A.java
@@ -0,0 +1,37 @@
+package ch01;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * Created by jun097kim on 2017-10-20.
+ */
+public class Q2_05_A {
+    private static int powerOf10 = 1;
+
+    public static void main(String[] args) {
+        LinkedList<Integer> digitList1 = new LinkedList<>(Arrays.asList(1, 5, 9));
+        LinkedList<Integer> digitList2 = new LinkedList<>(Arrays.asList(2, 3, 6, 7));
+
+        System.out.println(sumDigitLists(digitList1, digitList2));
+    }
+
+    private static int sumDigitLists(LinkedList<Integer> digitList1, LinkedList<Integer> digitList2) {
+        int sumFirstNodes = (removeFirstNode(digitList1) + removeFirstNode(digitList2)) * powerOf10;
+        powerOf10 *= 10;
+
+        if (digitList1.size() == 0 && digitList2.size() == 0) {
+            return sumFirstNodes;
+        }
+
+        return sumFirstNodes + sumDigitLists(digitList1, digitList2);
+    }
+
+    private static int removeFirstNode(LinkedList<Integer> digitList) {
+        if (!(digitList.size() > 0)) {
+            return 0;
+        }
+
+        return digitList.remove(0);
+    }
+}
---
+++ src/ch01/Q2_07_A.java
@@ -0,0 +1,49 @@
+package ch01;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * Created by jun097kim on 2017-10-21.
+ */
+public class Q2_07_A {
+    public static void main(String[] args) {
+        LinkedList<Integer> l1 = new LinkedList<>();
+        LinkedList<Integer> l2 = new LinkedList<>(Arrays.asList(9, 2));
+
+        LinkedList<Integer> intersection = new LinkedList<>(Arrays.asList(1, 7, 3, 5, 4));
+
+        l1.addAll(intersection);
+        l2.addAll(intersection);
+
+        System.out.println(findIntersection(l1, l2));
+    }
+
+    private static Integer findIntersection(LinkedList<Integer> l1, LinkedList<Integer> l2) {
+        Iterator<Integer> itL1 = l1.iterator();
+        Iterator<Integer> itL2 = l2.iterator();
+
+        if (l1.size() < l2.size()) {
+            nextDiff(itL2, l2.size() - l1.size());
+        } else if (l1.size() > l2.size()) {
+            nextDiff(itL1, l1.size() - l2.size());
+        }
+
+        while (itL1.hasNext()) {
+            Integer next1 = itL1.next();
+            Integer next2 = itL2.next();
+
+            if (next1 == next2) {
+                return next1;
+            }
+        }
+        return null;
+    }
+
+    private static void nextDiff(Iterator it, int diff) {
+        for (int i = 0; i < diff; i++) {
+            it.next();
+        }
+    }
+}
